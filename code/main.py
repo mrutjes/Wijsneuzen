06d0b3edf81b2dbbf7d9d7164e65037a06006e04
Max Rutjes <max.rutjes@student.uva.nl>
2025-01-09 13:16:51 +0100

    toevoegen van main functie

---
+++ code/main.py
@@ -0,0 +1,20 @@
+from grid_class import Grid_3D
+from nodes_class import Node
+from connections_class import Wire, WirePoint
+import matplotlib as plt
+
+
+#initialize the grid
+
+x = 4
+y = 5
+
+grid = Grid_3D(x, y)
+
+#initialize the nodes
+
+#initialize the netlist
+
+#make wires from the netlist
+
+#visialize the grid using matplotlib
