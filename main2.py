36516ae9f925c9efe7e2efc75928f8342cb7a5e1
Sven Geerlings <svengeerlings@sp-byods-145-109-36-236.wireless.uva.nl>
2025-01-17 14:56:05 +0100

    main voor permutaties netlist

---
+++ main2.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from code.classes.grid_class import Grid_3D, plot_wires_3d
+from code.classes.nodes_class import Node
+from code.classes.wire_class import Wire, WirePoint
+import pandas as pd
+from code.imports import import_netlist, import_nodes
+from code.algorithms.manhattan_algorithm import manhattan_wire
+import itertools
+
+nodes_csv_path = './gates&netlists/chip_0/print_0.csv'
+netlist_csv_path = './gates&netlists/chip_0/netlist_2.csv'
+grid_width = 10
+grid_length = 10
+functie = manhattan_wire
+
+
+# Initiate the grid, and import nodes and netlist
+grid = Grid_3D(grid_width, grid_length, nodes_csv_path)
+nodes_list = import_nodes(nodes_csv_path)
+for node in nodes_list:
+    grid.place_node(node)
+netlist = import_netlist(netlist_csv_path)
+all_wire_runs = []
+
+for netlists in itertools.permutations(netlist): 
+    # Initiate the wires
+    grid.clear_wires()
+    wires = []
+    if len(netlist) >= 1:
+        # Form the wires between the nodes based on the given netlist
+        wires = grid.return_wire_list()
+        for i in range(0, len(netlist)):
+            node1 = netlist[i][0]
+            node2 = netlist[i][1]
+
+            node1 = nodes_list[node1 - 1]
+            node2 = nodes_list[node2 - 1]
+
+            wire = functie(node1, node2, grid, nodes_csv_path, netlist_csv_path)
+
+            grid.add_wire_list(wire)
+        
+        all_wire_runs.append(wires)
+
+        # Plot the wires
+        plot_wires_3d(wires, grid_width, grid_length)
+
+        # Remove the nodes from the wires dict
+        grid.remove_nodes_pointdict()
+
+        # Calculate the cost of the grid
+        print(f"The total cost for this grid is: {grid.cost()}")
+
+    else:
+        raise ValueError("No netlist given.")
