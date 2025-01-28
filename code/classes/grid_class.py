6dc9b185c0fea7d84781dc38e140b1b4e914471d
Max Rutjes <max.rutjes@student.uva.nl>
2025-01-28 16:26:38 +0100

    aanpassen grid bounds

--- code/classes/grid_class.py
+++ code/classes/grid_class.py
@@ -498,8 +498,8 @@ class Grid_3D:
     
         
 def initialise_grid(nodes_list, nodes_csv_path, algorithm: str, netlist_csv_path):
-    grid_width = max(node.give_x() for node in nodes_list) + 2 # +2 for the 0-indexing and the extra space
-    grid_length = max(node.give_y() for node in nodes_list) + 2 # +2 for the 0-indexing and the extra space
+    grid_width = max(node.give_x() for node in nodes_list) + 1 # +1 for the 0-indexing
+    grid_length = max(node.give_y() for node in nodes_list) + 1 # +1 for the 0-indexing
     grid = Grid_3D(grid_width, grid_length, nodes_csv_path=nodes_csv_path, netlist_csv_path=netlist_csv_path)
     for node in nodes_list:
         grid.place_node(node)
